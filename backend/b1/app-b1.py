ae1fb4adbe84cb26cef84f41e68efd9661ed4423
Jaroslaw Krawczynski <jkraw@dmcs.pl>
2021-09-27 13:03:22 +0200

    first commit

---
+++ backend/b1/app-b1.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/',endpoint='/', methods=["GET"] )
+@app.route('/healthz',endpoint='healthz', methods=["GET"])
+def hello_world():
+    
+    if request.endpoint == '/':
+    	msg = 'backend 1 !!!!!!!!\n'
+    elif request.endpoint == 'healthz':
+    	msg = ''
+
+    return msg
